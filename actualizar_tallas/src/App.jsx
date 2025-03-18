1ccb1d115e07673b48050a3cafc6db7771b47be1
luis-chani <luis.chani.ag@gmail.com>
2025-03-18 09:17:08 -0500

    Add files via upload

---
+++ actualizar_tallas/src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import ActualizarTallas from "./pages/ActualizarTallas";
+
+function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-xl font-bold mb-4">Actualización de Tallas</h1>
+      <ActualizarTallas />
+    </div>
+  );
+}
+
+export default App;
